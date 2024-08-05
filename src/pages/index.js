4500c3175da75c73698c5e1810feac4a78e38d35
kennethwn <kenthnov117@gmail.com>
2024-08-05 20:29:01 +0700

    refactor: change button's prop from type to appearance

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,12 +2,11 @@ import { Inter } from "next/font/google";
 import Layouts from "@/components/Layouts";
 import Input from "@/components/Input";
 import { useState } from "react";
-import Button from "@/components/Button";
 import PrescribeIcon from "@/components/Icons/PrescribeIcon";
 import ContentLayout from "@/components/Layouts/Content";
 import { IoMdArrowDropleft as ArrowLeftIcon } from "react-icons/io";
-import useUser from "./api/user";
 import { useUserContext } from "./api/context/UserContext";
+import Button from "@/components/Button";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -22,8 +21,8 @@ export default function Home() {
           <Input type="text" id="name" name="name" onChange={(e) => setInput(e.target.value)} placeholder="name" />
         </div>
         <div className="w-1/4 mt-2">
-          <Button type='primary' className='w-1 mr-9'>{<ArrowLeftIcon size={'24px'} />}</Button>
-          <Button type="primary" onClick={() => alert(input)} appendIcon={<PrescribeIcon stroke="white" />}>Dimas</Button>
+          <Button appearance='primary' className='w-1 mr-9'>{<ArrowLeftIcon size={'24px'} />}</Button>
+          <Button appearance="primary" onClick={() => alert(input)} appendIcon={<PrescribeIcon stroke="white" />}>Dimas</Button>
           {/* <Button appearance="primary" color="violet" onClick={() => alert(input)}>Dimas</Button>  */}
         </div>
       </ContentLayout>
