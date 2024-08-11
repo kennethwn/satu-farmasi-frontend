74780cf31f5a2ac0586e563431aeb4dcfb3d25e1
kennethwn <kenthnov117@gmail.com>
2024-08-11 11:55:54 +0700

    refactor: move from directory component into src component

---
+++ src/configs/axios/satufarmasi-service-axios.jsx
@@ -0,0 +1,18 @@
+import axios from "axios";
+import errorHandler from "../errorHandler";
+
+const instance = axios.create({
+    baseURL: `${process.env.NEXT_PUBLIC_SATUFARMASI_API_URL}`,
+    withCredentials: true
+}, {
+    headers: {
+        "Access-Control-Allow-Origin": "http://localhost:8000",
+        "Access-Control-Allow-Methods": "GET,PUT,POST,DELETE,PATCH,OPTIONS",
+        "Accept": "application/json",
+        "Content-Type": "application/json",
+        "X-Requested-With": "XMLHttpRequest",
+    }
+});
+
+instance.interceptors.response.use((response) => response.data, errorHandler);
+export default instance;
