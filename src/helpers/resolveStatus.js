c4fc48ec3a0db00664b9cd8b47f625fdf87bfb5c
kennethwn <kenthnov117@gmail.com>
2024-11-16 13:48:29 +0700

    fix(resolveStatus): change color styling

--- src/helpers/resolveStatus.js
+++ src/helpers/resolveStatus.js
@@ -2,7 +2,7 @@ export const resolveIsPaidStatus = (status) => {
     if (status) {
         return (
             <div className="flex w-full">
-                <div className="w-full px-2 py-1 text-base font-medium text-green-500 bg-green-100">
+                <div className="w-full px-2 py-1 text-base font-medium" style={{ color: '#22c55e', backgroundColor: '#dcfce7' }}>
                     <div className="flex-initial max-w-full text-sm font-semibold leading-none text-center">Lunas</div>
                 </div>
             </div>
@@ -10,7 +10,7 @@ export const resolveIsPaidStatus = (status) => {
     } else {
         return (
             <div className="flex w-full">
-                <div className="w-full px-2 py-1 text-base font-medium text-red-500 bg-red-100">
+                <div className="w-full px-2 py-1 text-base font-medium" style={{ color: '#ef4444', backgroundColor: '#fee2e2' }}>
                     <div className="flex-initial max-w-full text-sm font-semibold leading-none text-center">Belum Lunas</div>
                 </div>
             </div>
