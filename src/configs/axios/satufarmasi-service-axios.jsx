8ba202196fcb98d8cb8d29a07401fb39d56ea419
kennethwn <kenthnov117@gmail.com>
2024-12-14 10:26:49 +0700

    refactor: change axios header request

--- src/configs/axios/satufarmasi-service-axios.jsx
+++ src/configs/axios/satufarmasi-service-axios.jsx
@@ -3,19 +3,13 @@ import errorHandler from "../errorHandler";
 
 const instance = axios.create({
     baseURL: `${process.env.NEXT_PUBLIC_SATUFARMASI_API_URL}`,
-    withCredentials: true
-}, {
+    withCredentials: true,
     headers: {
-        // "Access-Control-Allow-Origin": "http://localhost:8000",
-        "Access-Control-Allow-Origin": "https://satu-farmasi-backend.onrender.com",
-        'Access-Control-Allow-Credentials': true,
-        "Access-Control-Allow-Methods": "GET,PUT,POST,DELETE,PATCH,OPTIONS",
-        'Access-Control-Allow-Headers': 'Origin, Content-Type, X-Auth-Token, X-Requested-With, Authorization, Accept',
         "Accept": "application/json",
         "Content-Type": "application/json",
         "X-Requested-With": "XMLHttpRequest",
     }
-});
+})
 
 instance.interceptors.response.use((response) => response.data, errorHandler);
 export default instance;
